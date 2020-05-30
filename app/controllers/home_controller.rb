e085e0250c0941f4f3b950ec05a5837a535706ac
Bishwas Thapa Magar <bishwastmgr@gmail.com>
2020-05-30 14:02:27 +0545

    changes need to be made

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if params[:checker] == ""
+      @nothing = "You forget to Enter"
+    elsif params[:checker]
+      @api = StockQuote::Stock.new(api_key: 'pk_82131e29bd244d589b1e466913b36f80')
+      @data = StockQuote::Stock.quote(params[:checker])
+      if !@data
+        @error = "Error"
+      end
+    end
   end
 
   def about
